509523241bd193e7a4d5fd05677dd8d6affb44a0
Yi <z1joey@icloud.com>
2019-04-24 03:17:17 +0800

    welcome view controller added

---
+++ iChat/WelcomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WelcomeViewController.swift
+//  iChat
+//
+//  Created by Yi Zhang on 2019/4/24.
+//  Copyright © 2019 Yi Zhang. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var repeatPasswordTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: IBAction
+    
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        print("login")
+    }
+    
+    @IBAction func registerButtonTapped(_ sender: Any) {
+        print("register")
+    }
+    
+    @IBAction func backgroundTapped(_ sender: Any) {
+        print("dismiss")
+    }
+}
